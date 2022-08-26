ddee80b3a544cfa234df546a820279cceddf4a08
NicolasBenitez30 <nicolasbenitez1438@gmail.com>
2022-08-26 17:51:08 -0300

    nuevo

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ let playlists = [
         descripcion: "chill's track's",
         canciones: [
             {
-                nombre: "Perla",
+                titulo: "Perla",
                 artista: "C.R.O",
                 album: "Perla",
                 anio: "2018"
             },
             {
-                nombre: "Me voy a olvidar",
+                titulo: "Me voy a olvidar",
                 artista: "T&K",
                 album: "El Libro Negro",
                 anio: "2018"
             },
             {
-                nombre: "Ly$",
+                titulo: "Ly$",
                 artista: "C.R.O & Franky Style",
                 album: "MBD Crew",
                 anio: "2018"
@@ -49,16 +49,83 @@ app.get('/lists', (request, response) => {
 
 app.get('/lists/:nombre', (request, response) => {
     let nombre = request.params.nombre
-    let playlistN = response.send(playlists.filter(x => x.nombre == nombre))
-    if (playlistN.nombre == null) {
+    let playlist = playlists.find(x => x.nombre == nombre)
+    if (playlist == null) {
         response.status(400).send("nombre de lista incorrecto")
+        return
     }
-    let playlistD = request.params.descripcion
-    if (playlistD.descripcion == null) {
-        response.status(400).send("descripcion de la lista no existe")
-    }
-    response.send(playlistN, playlistD)
+    response.send(playlist)
 })
+
+app.put('/lists/:nombre', (request, response) => {
+    let nombre = request.params.nombre
+    let playlist = playlists.find(x => x.nombre == nombre)
+    if (playlist == null) {
+        response.status(400).send("nombre de lista incorrecto")
+        return
+    }
+    playlist.descripcion = request.body.descripcion
+    response.send(playlist)
+})
+
+app.delete('/lists/:nombre', (request, response) => {
+    let nombre = request.params.nombre
+    let playlist = playlists.find(x => x.nombre == nombre)
+    if (playlist == null) {
+        response.status(400).send("nombre de lista incorrecto")
+        return
+    }
+    let eliminar = playlists.indexOf(playlist)
+    playlists.splice(eliminar, 1)
+    response.send("Se elimino la Playlist")
+
+})
+
+app.get('/lists/:nombre/songs', (request, response) => {
+    let nombre = request.params.nombre
+    let playlist = playlists.find(x => x.nombre == nombre)
+    if (playlist == null) {
+        response.status(400).send("nombre de lista incorrecto")
+        return
+    }
+    response.send(playlist.canciones)
+})
+
+app.get('/lists/:nombre/songs/:titulo', (request, response) => {
+    let nombre = request.params.nombre
+    let playlist = playlists.find(x => x.nombre == nombre)
+    if (playlist == null) {
+        response.status(400).send("nombre de lista incorrecto")
+        return
+    }
+    let titulo = request.params.titulo
+    let cancion = playlist.canciones.find(x => x.titulo == titulo)
+    if (cancion == null) {
+        response.status(400).send("nombre de cancion incorrecto")
+        return
+    }
+    response.send(cancion)
+})
+
+app.post('/lists/:nombre/songs', (request, response) => {
+    let nombre = request.params.nombre
+    let playlist = playlists.find(x => x.nombre == nombre)
+    if (playlist == null) {
+        response.status(404).send("nombre de lista incorrecto")
+        return
+    }
+    let cancion = request.body
+    if (cancion.titulo == null) {
+        response.status(400).send("nombre de cancion incorrecto")
+        return
+    }
+    playlist.canciones.push(request.body)
+    response.status(201).send(playlist.canciones)
+})
+
+// app.put('', (request, response) => {
+
+// })
 
 app.listen(port, () => {
     console.log(`Escuchando peticiones en ${port}`)
