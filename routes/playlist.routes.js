5b155e7fde2cf608afa3e60df4eef4376b37dc6f
NicolasBenitez30 <nicolasbenitez1438@gmail.com>
2022-11-28 01:42:10 -0300

    se supone q terminada

--- routes/playlist.routes.js
+++ routes/playlist.routes.js
@@ -2,134 +2,121 @@ import express from 'express'
 
 const router = express.Router()
 
-import Playlist from '../models/playlist.model'
+import Playlist from '../models/playlist.model.js'
 
 //endpoints
 
-router.post('/lists', (req, res) => {
-    let playlist = req.body
-    if (playlist.nombre == null) {
-        res.status(400).send("nombre de lista incorrecto")
+router.post('/lists', async (req, res) => {
+    try {
+        const playlist = req.body
+        await Playlist.create(playlist)
+        res.status(201).send(playlist)
+    } catch (err) {
+        res.status(500).send(err)
     }
-    playlists.push(req.body)
-    res.status(201).send(playlist)
 })
 
-    .get('/lists', (req, res) => {
-    res.send(playlists)
+router.get('/lists', async (req, res) => {
+    try {
+        const playlist = await Playlist.find()
+        res.send(playlists)
+    } catch (err) {
+        res.status(500).send(err)
+    }
 })
 
-router.get('/lists/:nombre', (req, res) => {
-    let nombre = req.params.nombre
-    let playlist = playlists.find(x => x.nombre == nombre)
-    if (playlist == null) {
-        res.status(400).send("nombre de lista incorrecto")
-        return
+router.get('/lists/:nombre', async (req, res) => {
+    try {
+        let nombrePlaylist = req.params.nombre
+        const playlist = Playlist.findOne({ nombre: nombrePlaylist})
+        res.send(playlist)
+    } catch (err) {
+        res.status(500).send(err)
     }
-    res.send(playlist)
 })
 
-router.put('/lists/:nombre', (req, res) => {
-    let nombre = req.params.nombre
-    let playlist = playlists.find(x => x.nombre == nombre)
-    if (playlist == null) {
-        res.status(400).send("nombre de lista incorrecto")
-        return
+router.put('/lists/:nombre', async (req, res) => {
+    try {
+        let nombrePlaylist = req.params.nombre
+        let playlist = req.body
+        await Playlist.findOneAndUpdate({ nombre: nombrePlaylist}, playlist)
+        const playlistResponse = await Playlist.findOne({ nombre: nombrePlaylist})
+        res.send(playlistResponse)
+    } catch (err) {
+        res.status(500).send(err)
     }
-    playlist.descripcion = req.body.descripcion
-    res.send(playlist)
 })
 
-router.delete('/lists/:nombre', (req, res) => {
-    let nombre = req.params.nombre
-    let playlist = playlists.find(x => x.nombre == nombre)
-    if (playlist == null) {
-        res.status(400).send("nombre de lista incorrecto")
-        return
+router.delete('/lists/:nombre', async (req, res) => {
+    try {
+        let nombrePlaylist = req.params.nombre
+        await Playlist.findOneAndRemove({ nombre: nombrePlaylist})
+        res.status(204).send()
+    } catch (err) {
+        res.status(500).send(err)
     }
-    let eliminar = playlists.indexOf(playlist)
-    playlists.splice(eliminar, 1)
-    res.send("Se elimino la Playlist")
-
 })
 
-router.get('/lists/:nombre/songs', (req, res) => {
-    let nombre = req.params.nombre
-    let playlist = playlists.find(x => x.nombre == nombre)
-    if (playlist == null) {
-        res.status(400).send("nombre de lista incorrecto")
-        return
+router.get('/lists/:nombre/songs', async (req, res) => {
+    try {
+        let nombrePlaylist = req.params.nombre
+        const playlist = await Playlist.findOne({ nombre: nombrePlaylist})
+        res.send(playlist.canciones)
+    } catch (err) {
+        res.status(500).send(err)
     }
-    res.send(playlist.canciones)
 })
 
-router.get('/lists/:nombre/songs/:titulo', (req, res) => {
-    let nombre = req.params.nombre
-    let playlist = playlists.find(x => x.nombre == nombre)
-    if (playlist == null) {
-        res.status(400).send("nombre de lista incorrecto")
-        return
+router.get('/lists/:nombre/songs/:titulo', async (req, res) => {
+    try {
+        let nombrePlaylist = req.params.nombre
+        const playlist = await Playlist.findOne({ nombre: nombrePlaylist})
+        let tituloCancion = req.params.titulo
+        const cancion = await Playlist.canciones.findOne({ titulo: tituloCancion})
+        res.send(cancion)
+    } catch (err) {
+        res.status(500).send(err)
     }
-    let titulo = req.params.titulo
-    let cancion = playlist.canciones.find(x => x.titulo == titulo)
-    if (cancion == null) {
-        res.status(400).send("nombre de cancion incorrecto")
-        return
-    }
-    res.send(cancion)
 })
 
-router.post('/lists/:nombre/songs', (req, res) => {
-    let nombre = req.params.nombre
-    let playlist = playlists.find(x => x.nombre == nombre)
-    if (playlist == null) {
-        res.status(404).send("nombre de lista incorrecto")
-        return
+router.post('/lists/:nombre/songs', async (req, res) => {
+    try {
+        let nombrePlaylist = req.params.nombre
+        const playlist = await Playlist.findOne({ nombre: nombrePlaylist})
+        const cancion = req.body
+        await Playlist.cancion.create(cancion)
+        res.status(201).send(cancion)
+    } catch (err) {
+        res.status(500).send(err)
     }
-    let cancion = req.body
-    if (cancion.titulo == null) {
-        res.status(400).send("nombre de cancion incorrecto")
-        return
-    }
-    playlist.canciones.push(req.body)
-    res.status(201).send(playlist.canciones)
 })
 
-router.put('/lists/:nombre/songs/:titulo', (req, res) => {
-    let nombre = req.params.nombre
-    let playlist = playlists.find(x => x.nombre == nombre)
-    if (playlist == null) {
-        res.status(404).send("nombre de lista incorrecto")
-        return
+router.put('/lists/:nombre/songs/:titulo', async (req, res) => {
+    try {
+        let nombrePlaylist = req.params.nombre
+        const playlist = await Playlist.findOne({ nombre: nombrePlaylist})
+        let tituloCancion = req.body
+        await Playlist.canciones.cancion.findOneAndUpdate({ titulo: tituloCancion}, cancion)
+        await cancion.artista.findOneAndUpdate({ titulo: tituloCancion})
+        await cancion.album.findOneAndUpdate({ titulo: tituloCancion})
+        await cancion.anio.findOneAndUpdate({ titulo: tituloCancion})
+        const cancionResponse = await Playlist.findOne({ titulo: tituloCancion})
+        res.send(cancionResponse)
+    } catch (err) {
+        res.status(500).send(err)
     }
-    let titulo = req.params.titulo
-    let cancion = playlist.canciones.find(x => x.titulo == titulo)
-    if (cancion.titulo == null) {
-        res.status(400).send("nombre de cancion incorrecto")
-        return
-    }
-    cancion.artista = req.body.artista
-    cancion.album = req.body.album
-    cancion.anio = req.body.anio
-    res.send(cancion)
 })
 
-router.delete('/lists/:nombre/songs/:titulo', (req, res) => {
-    let nombre = req.params.nombre
-    let playlist = playlists.find(x => x.nombre == nombre)
-    if (playlist == null) {
-        res.status(404).send("nombre de lista incorrecto")
-        return
+router.delete('/lists/:nombre/songs/:titulo', async (req, res) => {
+    try {
+        let nombrePlaylist = req.params.nombre
+        let playlist = await Playlist.findOne({ nombre: nombrePlaylist})
+        let tituloCancion = req.params.titulo
+        await Playlist.canciones.cancion.findOneAndRemove({ titulo: tituloCancion})
+        res.status(204).send()
+    } catch (err) {
+        res.status(500).send(err)
     }
-    let titulo = req.params.titulo
-    let cancion = playlist.canciones.find(x => x.titulo == titulo)
-    if (cancion.titulo == null) {
-        res.status(400).send("nombre de cancion incorrecto")
-        return
-    }
-    let eliminar = playlist.canciones.indexOf(cancion)
-    playlist.canciones.splice(eliminar, 1)
-    res.send("Se elimino la Cancion")
 })
 
 export default router
